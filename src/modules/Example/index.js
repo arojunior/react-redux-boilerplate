dae784d2da320b09d595ddfd2b43686de813db14
Junior Oliveira <arojunior@gmail.com>
2017-04-07 11:50:21 -0300

    module example refactored

--- src/modules/Example/index.js
+++ src/modules/Example/index.js
@@ -1,48 +1,29 @@
-import axios from 'axios'
-import {createAction} from 'redux-actions'
+import {handleActions} from 'redux-actions'
 
-const GITHUB_FETCHING = 'modules/Github/FETCHING'
-const GITHUB_SUCCESS  = 'modules/Github/SUCCESS'
-const GITHUB_ERROR    = 'modules/Github/ERROR'
-
-export const githubFetching = createAction(GITHUB_FETCHING)
-export const githubError    = createAction(GITHUB_ERROR)
-export const githubSuccess  = createAction(GITHUB_SUCCESS)
+import {GITHUB_FETCHING, GITHUB_SUCCESS, GITHUB_ERROR} from './actions'
 
 const initialState = {
     fetching : false,
     user     : null
 }
 
-export default (state = initialState, action) => {
-    switch (action.type) {
-        case GITHUB_FETCHING:
-            return {...state,
-                    fetching     : true
-                    }
-        case GITHUB_SUCCESS:
-            return {...state,
-                    fetching     : false,
-                    user         : action.payload.data
-                  }
-        case GITHUB_ERROR:
-          return {...state,
-                  fetching       : false
-                }
-      default:
-        return state;
-    }
-}
+const reducer = handleActions({
+  [GITHUB_FETCHING]: (state, action) => ({
+      ...state,
+      sending     : true
+  }),
 
-export const getUserFromGithub = username => {
-    return {
-        type : [
-            githubFetching,
-            githubSuccess,
-            githubError
-        ],
-        payload : {
-            data : () => axios.get('https://api.github.com/users/' + username)
-        }
-    }
-}
+  [GITHUB_SUCCESS]: (state, action) => ({
+      ...state,
+      sending     : false,
+      user        : action.payload.data
+  }),
+
+  [GITHUB_ERROR]: (state, action) => ({
+      ...state,
+      sending     : false,
+  })
+
+}, initialState);
+
+export default reducer
